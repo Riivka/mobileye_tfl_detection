eca481379f7a13542e98be63a412678d6e53106c
Tamar-Ba <tamarbahaj@gmail.com>
2021-01-06 11:19:18 +0200

    visalize

--- tfl_man.py
+++ tfl_man.py
@@ -39,7 +39,24 @@ class TflMan:
         sec_part_1.plot([1, 2, 3])
         sec_part_2.plot([1, 2, 3])
         sec_part_3.plot([1, 2, 3])
-        # sec_part_1.imshow(img_part_1)
-        # sec_part_2.imshow(img_part_2)
-        # sec_part_3.imshow(img_part_3)
+        sec_part_1.imshow(frame.image)
+        sec_part_2.imshow(frame.image)
+        sec_part_3.imshow(frame.image)
+        plt.show()
+        fig, (sec_part_1, sec_part_2, sec_part_3) = plt.subplots(3, sharex=True, sharey=True, figsize=(12, 6))
+        fig.suptitle(f'Frame #{frame.frame_id}: image name')
+        sec_part_1.imshow(frame.image)
+        x = [pt[0] for pt in frame.candidates]
+        y = [pt[1] for pt in frame.candidates]
+        sec_part_1.plot(x, y, 'ro', marker='o', color='r', markersize=2)
+        sec_part_2.imshow(frame.image)
+        sec_part_3.imshow(frame.image)
+        # curr_sec.plot(curr_p[:, 0], curr_p[:, 1], 'b+')
+        for i in range(len(frame.traffic_lights)):
+            # curr_sec.plot([curr_p[i, 0], foe[0]], [curr_p[i, 1], foe[1]], 'b')
+            if frame.valid[i]:
+                sec_part_3.text(frame.traffic_lights[i][0], frame.traffic_lights[i][1],
+                                r'{0:.1f}'.format(frame.distances[i]), color='r')
+        # curr_sec.plot(foe[0], foe[1], 'r+')
+        # curr_sec.plot(rot_pts[:, 0], rot_pts[:, 1], 'g+')
         plt.show()
